7f9eeedc225c7fa63342f7a7a13a4f244b4941d0
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2024-10-10 17:24:17 +0530

    add to cart functionality working

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,29 +1,46 @@
-export const Card = ({ foodItemData }) => {
-  console.log(foodItemData, "foodItemData");
-  return (
-    <div className="max-w-sm p-5 rounded border border-black flex flex-col items-center justify-center bg-white shadow-lg overflow-hidden">
-      <img
-        className="w-55 min-h-52 object-fit"
-        src="/logo.jpg"
-        alt="foodImage"
-      />
+import useCartStore from "../../store/useCartStore";
 
-      <div className="p-4 flex justify-between items-center ">
-        <div className="flex flex-col justify-between gap-2">
-          <div className="flex justify-between items-center  ">
-            <h3 className="text-2xl">food name</h3>
-            <div>
-              <span className="text-2xl">$20</span>
+export const Card = ({ foodItemData }) => {
+  const { name, price, description } = foodItemData;
+  const { addItem } = useCartStore();
+  const handleAddToCart = (itemAdded) => {
+    addItem(foodItemData);
+  };
+
+  return (
+    <>
+      {foodItemData && (
+        <div className="max-w-sm p-5 rounded border border-black flex flex-col items-center justify-between bg-white shadow-lg overflow-hidden">
+          <img
+            className="w-55 min-h-52 object-fit"
+            src="/logo.jpg"
+            alt="foodImage"
+          />
+
+          <div className="p-2 flex items-center ">
+            <div className="flex flex-col justify-between gap-2">
+              <div className="flex justify-between items-center  ">
+                <h3 className="text-2xl">{name}</h3>
+                <div>
+                  <span className="text-2xl">${price}</span>
+                </div>
+              </div>
+              <p className="line-clamp-2 hover:line-clamp-none transition-all duration-300 ease-in-out">
+                {description}
+              </p>
+              <div className="flex justify-end items-center mt-4  ">
+                <button
+                  onClick={() => {
+                    handleAddToCart(foodItemData);
+                  }}
+                >
+                  Add to Cart
+                </button>
+              </div>
             </div>
           </div>
-          <p>
-            Fresh tuna cubes with rice, seaweed, avocado, and soy sauce dressing
-          </p>
-          <div className="flex justify-end items-center mt-4  ">
-            <button>Add to Cart</button>
-          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
--- src/pages/Cartpage.tsx
+++ src/pages/Cartpage.tsx
@@ -6,27 +6,41 @@ export const Cartpage = () => {
   return (
     <div className="container mx-auto p-8">
       <h2 className="text-3xl font-bold mb-8">Your Shopping Cart</h2>
-      <div className="space-y-6">
-        <div className="flex justify-between items-center p-4 border-b border-gray-200 overflow-hidden">
-          <div className="flex">
-            <img className="h-20 object-cover " src="/logo.jpg" alt="logo" />
-            <div>
-              <h3 className="text-xl font-semibold">itemName</h3>
-              <p className="text-gray-500">$55 each</p>
-              <p className="text-gray-700">Quantity: 4</p>
-              <div className="flex mt-2 space-x-2">
-                <button className="bg-gray-200 px-3 py-1 rounded hover:bg-gray-300 transition-colors">
-                  -
-                </button>
-                <button className="bg-gray-200 px-3 py-1 rounded hover:bg-gray-300 transition-colors">
-                  +
-                </button>
+      {items.length > 0 ? (
+        items.map((item) => {
+          return (
+            <div className="space-y-6">
+              <div className="flex justify-between items-center p-4 border-b border-gray-200 overflow-hidden">
+                <div className="flex">
+                  <img
+                    className="h-20 object-cover "
+                    src="/logo.jpg"
+                    alt="logo"
+                  />
+                  <div>
+                    <h3 className="text-xl font-semibold">{item.name}</h3>
+                    <p className="text-gray-500">${item.price} each</p>
+                    <p className="text-gray-700">Quantity: {item.quantity}</p>
+                    <div className="flex mt-2 space-x-2">
+                      <button className="bg-gray-200 px-3 py-1 rounded hover:bg-gray-300 transition-colors">
+                        -
+                      </button>
+                      <button className="bg-gray-200 px-3 py-1 rounded hover:bg-gray-300 transition-colors">
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <p className="text-xl font-semibold">
+                  {item.price * item.quantity}
+                </p>
               </div>
             </div>
-          </div>
-          <p className="text-xl font-semibold">555</p>
-        </div>
-      </div>
+          );
+        })
+      ) : (
+        <div>Shoping Cart is Empty</div>
+      )}
 
       <div className="mt-8 p-6 bg-gray-100 rounded-lg shadow-md">
         <h3 className="text-2xl font-bold mb-4">Total:</h3>
